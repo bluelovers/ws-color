c1f3211f3c493ad7b178636946ec3b50f523b7c7
bluelovers <codelovers@users.sourceforge.net>
2023-01-29 05:11:54 +0800

    init: @lazy-color/palette-apexcharts

---
+++ packages/@lazy-color/palette-apexcharts/README.md
@@ -0,0 +1,12 @@
+# README.md
+
+    
+
+## install
+
+```bash
+yarn add @lazy-color/palette-apexcharts
+yarn-tool add @lazy-color/palette-apexcharts
+yt add @lazy-color/palette-apexcharts
+```
+
---
+++ packages/@lazy-color/palette-apexcharts/dist/index.d.ts
@@ -0,0 +1,88 @@
+/**
+ * @see https://github.com/apexcharts/apexcharts.js/blob/main/src/modules/Theme.js#L198
+ */
+export declare const paletteApexcharts: {
+	readonly default: readonly [
+		"#008FFB",
+		"#00E396",
+		"#FEB019",
+		"#FF4560",
+		"#775DD0"
+	];
+	readonly palette1: readonly [
+		"#008FFB",
+		"#00E396",
+		"#FEB019",
+		"#FF4560",
+		"#775DD0"
+	];
+	readonly palette2: readonly [
+		"#3f51b5",
+		"#03a9f4",
+		"#4caf50",
+		"#f9ce1d",
+		"#FF9800"
+	];
+	readonly palette3: readonly [
+		"#33b2df",
+		"#546E7A",
+		"#d4526e",
+		"#13d8aa",
+		"#A5978B"
+	];
+	readonly palette4: readonly [
+		"#4ecdc4",
+		"#c7f464",
+		"#81D4FA",
+		"#fd6a6a",
+		"#546E7A"
+	];
+	readonly palette5: readonly [
+		"#2b908f",
+		"#f9a3a4",
+		"#90ee7e",
+		"#fa4443",
+		"#69d2e7"
+	];
+	readonly palette6: readonly [
+		"#449DD1",
+		"#F86624",
+		"#EA3546",
+		"#662E9B",
+		"#C5D86D"
+	];
+	readonly palette7: readonly [
+		"#D7263D",
+		"#1B998B",
+		"#2E294E",
+		"#F46036",
+		"#E2C044"
+	];
+	readonly palette8: readonly [
+		"#662E9B",
+		"#F86624",
+		"#F9C80E",
+		"#EA3546",
+		"#43BCCD"
+	];
+	readonly palette9: readonly [
+		"#5C4742",
+		"#A5978B",
+		"#8D5B4C",
+		"#5A2A27",
+		"#C4BBAF"
+	];
+	readonly palette10: readonly [
+		"#A300D6",
+		"#7D02EB",
+		"#5653FE",
+		"#2983FF",
+		"#00B1F2"
+	];
+};
+
+export {
+	paletteApexcharts as default,
+};
+
+export {};
